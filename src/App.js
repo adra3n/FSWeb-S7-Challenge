a2d3c66ce185f19fe41963b68ea29a88280dad3a
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-03-14 20:11:16 +0300

    refactor edildi. checkbox yup calisiyor. cypress eklenecek.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Form from "./components/Form";
 
 const App = () => {
   return (
     <>
       <h1>Teknolojik Yemekler</h1>
       <p>Burdaki kodu silip kendi headerınızı ekleyebilirsiniz</p>
+      <Routes>
+        <Route path="/pizza" element={<Form />} />
+      </Routes>
     </>
   );
 };
